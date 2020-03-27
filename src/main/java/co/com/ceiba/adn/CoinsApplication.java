7d8a311ca3ed6b32a9df8854241e011bb04e9c02
manualed <manuel.durango@ceiba.com.co>
2020-03-27 11:17:19 -0500

    Commit inicial - estructura proyecto basica

---
+++ src/main/java/co/com/ceiba/adn/CoinsApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.adn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinsApplication.class, args);
+	}
+
+}
